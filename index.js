0a40715f14bfed4890db9f33d10ef1e8210eaa13
Briana Oursler <boursler@blackhole.localdomain>
2018-04-17 15:52:13 -0700

    Add comments for code readability

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var guesses_left = 7;
 var word = new Word("hello");
 var word_bank = ["Saruman", "Aragorn", "Sauron", "Morgoth", "Valaquenta", "Galadriel", "hobbit", "Teleri", "Beren"];
 function make_word() {
+	//select random word from word bank and run through Word constructor
 	var word_index = Math.floor(Math.random() * (word_bank.length + 1));
 	word = new Word(word_bank[word_index]);
-	console.log("word is " + word);
+
 }
 //check if there are any underscores -- if underscores decreases, guess is correct
 //if it does not, guess is incorrect
@@ -20,7 +21,7 @@ var prompt = function () {
 		}
 	}
 	console.log(guesses_left + " guesses left " + underscores_count + " underscores count");
-
+	//check end game conditions and store underscores_count from before a guess is submitted
 	if (guesses_left > 0 && underscores_count > 0) {
 
 		inquirer.prompt([
@@ -29,6 +30,7 @@ var prompt = function () {
 				message: "Guess a letter!"
 			}
 		]).then(function (answers) {
+			//store a new count after guess submission for comparison
 			var tmp_underscores_count = 0;
 			word.guess_letter(answers.guess);
 			for (let i = 0; i < word_length; i++) {
@@ -37,7 +39,6 @@ var prompt = function () {
 				}
 			}
 
-
 			if (underscores_count === tmp_underscores_count) {
 				guesses_left--;
 				console.log("You guessed wrong! " + "\n" + guesses_left + " guesses remaining");
@@ -45,9 +46,11 @@ var prompt = function () {
 			else {
 				console.log("You guessed right! " + "\n" + word.toString());
 			}
+			//prompt calls itself until end conditions are met
 			prompt();
 		});
 	}
+	//check whether player won or lost; restart game if player won. Player who lost is toast.
 	else if (guesses_left === 0)
 		console.log("Oh no, you've lost the game!");
 	else {
--- word.js
+++ word.js
@@ -1,18 +1,22 @@
 var Letter = require("./letter.js");
 
 var Word = function (word) {
+	//initialize empty letter array
 	this.letters_arr = [];
+	//run each letter through the word constructors and add to letters array
 	for (let i = 0; i < word.length; i++) {
 		var letter = new Letter(word[i]);
 		this.letters_arr.push(letter);
 	}
 	this.toString = function () {
+		//build displayed word
 		var displayed_word = "";
 		for (let i = 0; i < this.letters_arr.length; i++) {
 			displayed_word += this.letters_arr[i].toString();
 		}
 		return displayed_word;
 	}
+
 	this.guess_letter = function (guess) {
 		for (let i = 0; i < this.letters_arr.length; i++) {
 			this.letters_arr[i].guess(guess);
